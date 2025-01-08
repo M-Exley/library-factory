7d9d3c3ac6c5be8c2eb011ba3eb24b10582371fb
Macaylen <macaylen.exley@gmail.com>
2025-01-08 12:19:18 +0100

    logic completed

--- script.js
+++ script.js
@@ -21,58 +21,98 @@ let bookLibrary = [
 }];
 
 
-
-// 1. Display books if there are any in the library
-
-function UpdateLibrary (title, author, pages, read) {
-    
-        // display books already in array 
-    if (bookLibrary.length) { 
+(function UpdateLibrary (title, author, pages, read) {
+        // display books already in array {if}
+        const checkUI = function()  {    
+        if (bookLibrary.length) { 
         bookLibrary.forEach((book) => {
-        // console.table(book.title)
         const {title, author, pages, read} = book;
-        const resString = `${title}, by ${author} is ${pages} pages long. ${read}`
-        // console.log(resString)
-        // display book in DOM
+        const resString = `${title}, by ${author} is ${pages} pages long. ${read} `
         const newCardDiv = document.createElement('div');
         cards.append(newCardDiv);
         newCardDiv.classList.add('card');
         newCardDiv.textContent = resString;
-        
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete')
+        deleteButton.textContent = 'Delete Book'
+        newCardDiv.append(deleteButton);
+            
+        deleteButton.addEventListener('click', function() {
+            const parent = deleteButton.parentNode;
+            const toString = parent.textContent.split(',')[0]; // works
+           
+            for(let i = 0; i < bookLibrary.length; i++) {
+                const returnTitle = bookLibrary[i].title.toString();                
+                
+                if (toString === returnTitle) {                    
+                    const toStringIndex = bookLibrary.findIndex(book => book.title === toString);
+                
+                    if (toStringIndex !== -1) {
+                        console.log(`Removing: ${toString}`);
+                        bookLibrary.splice(toStringIndex, 1); 
+                        parent?.remove(); 
+                    } else {
+                        console.warn(`Title not found: ${toString}`);
+                    }
+                }
+                
+            }
             })
-    } else {
-        alert("no books in library")
-    }
 
-    // put DOM els into array and loop 
-    const bookLibraryTouch = document.querySelectorAll('.card')
-    const bookLibraryTouchArr = [...bookLibraryTouch];
-    bookLibraryTouchArr.forEach((div) => {
-    div.addEventListener('click', function() {
-        div.remove();
-        bookLibrary.splice(div, 1)
-        console.table(bookLibrary)
-    })
-   })
+        })
+
+            
+         {
+        }}  
+        
+    };
+        checkUI();
 
     // add new books > update UI each time
     const add = document.getElementById('add');
     add.addEventListener('click', function(e) {
-    e.preventDefault();
-    const titleInput = document.getElementById('title').value;
-    const authorInput = document.getElementById('author').value;
-    const pagesInput = Number(document.getElementById('pages').value);
-    const readInput = document.querySelector('input[name="read"]:checked').value;
-    title = titleInput;
-    author = authorInput;
-    pages = pagesInput;
-    read = readInput;
+        const titleInput = document.getElementById('title').value;
+        const authorInput = document.getElementById('author').value;
+        const pagesInput = Number(document.getElementById('pages').value);
+        const readInput = document.querySelector('input[name="read"]:checked').value;
+        e.preventDefault();
+        
+        (!titleInput || !authorInput)  ?  alert('enter details') :
 
-    
-    
-    bookLibrary.push(newBook)
-    console.table(bookLibrary)
-    
-    })
-}
-UpdateLibrary();
+        title = titleInput;
+        author = authorInput;
+        pages = pagesInput;
+        read = readInput;
+        
+        const newBookObject = {title, author, pages, read}
+        console.log(`Adding: ${title}`);
+        bookLibrary.push(newBookObject)
+        console.table(bookLibrary);
+        cards.replaceChildren()
+        UpdateLibrary();
+        form.reset();
+
+
+        // if (!titleInput  && !authorInput)
+          
+        //   {
+        //     console.log('enter details')}
+        //   else {
+        //   }
+
+        })
+        
+
+        const clearLibrary = document.getElementById('clear')
+        clearLibrary.addEventListener('click', function() {
+            bookLibrary = [];
+            cards.textContent = 'No books'
+            checkUI();
+        })
+
+       
+      
+})();
+
+
+  
